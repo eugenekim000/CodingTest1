69452659332c053d59e4c59550ed8c869aa972d7
eugenekim000 <eugenekim000@gmail.com>
2020-02-04 22:34:28 -0800

    Updates

---
+++ precache-manifest.1103f89152a899c5920554bf5661299a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51f00ec81e5700e1f8dabfd30740cc4",
+    "url": "/CodingTest1/index.html"
+  },
+  {
+    "revision": "ebcde866197c4c4b7d69",
+    "url": "/CodingTest1/static/css/main.8a534b0e.chunk.css"
+  },
+  {
+    "revision": "3bca39ae97651f968800",
+    "url": "/CodingTest1/static/js/2.c6089b24.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CodingTest1/static/js/2.c6089b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebcde866197c4c4b7d69",
+    "url": "/CodingTest1/static/js/main.b392f2dc.chunk.js"
+  },
+  {
+    "revision": "df29223d4ffed9ddbd6f",
+    "url": "/CodingTest1/static/js/runtime-main.29307fbb.js"
+  }
+]);
